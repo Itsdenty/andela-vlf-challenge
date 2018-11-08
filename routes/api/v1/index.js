8fd02a2408ab95cfbdaf16a86790eb3b78620ba9
Abd-afeez Abd-hamid <dent4real@yahoo.com>
2018-11-08 08:47:03 +0000

    Update: add route base code

---
+++ routes/api/v1/index.js
@@ -0,0 +1,45 @@
+import express from 'express';
+import swaggerJSDoc from 'swagger-jsdoc';
+import user from './user';
+
+const router = express.Router();
+
+
+// Swagger jsdoc configuration
+const swaggerDefinition = {
+  info: {
+    title: 'Node Swagger API',
+    version: '1.0.0',
+    description: 'Api documentation for the TSS inventory app.',
+  },
+  host: 'localhost:3100/',
+  basePath: '/api/v1',
+};
+
+
+// options for the swagger docs
+const options = {
+
+  // import swaggerDefinitions
+  swaggerDefinition,
+
+  // path to the API docs
+  apis: ['./swagger/*.js']
+
+};
+
+// initialize swagger-jsdoc
+const swaggerSpec = swaggerJSDoc(options);
+
+
+router.get('/swagger.json', (req, res) => {
+  res.header('Content-Type', 'application/json');
+  res.send(swaggerSpec);
+});
+
+router.get('/', (req, res) => {
+  res.send('You\'ve reached api/v1 routes');
+});
+router.use('/user', user);
+
+export default router;
---
+++ routes/api/v1/user.js
@@ -0,0 +1,11 @@
+import express from 'express';
+// import controller from '../../../controllers/user'
+
+// const router = express.Router();
+const router = express.Router();
+router.get('/', 'route is still under development please try again later');
+// router.post('/', controller.userCreate);
+// router.post('/login', controller.userLogin);
+// router.get('/logout', controller.userLogout);
+
+export default router;
