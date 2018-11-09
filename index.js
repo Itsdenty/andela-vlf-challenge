ebca556c0192dc267d1434af54322d8e42c94501
Abd-afeez Abd-hamid <dent4real@yahoo.com>
2018-11-09 06:40:35 +0000

    add seeders

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ import routes from './routes';
 
 
 const app = express(),
-    pool = new pg.Pool(dbConfig),
-    port = process.env.PORT;
+  pool = new pg.Pool(dbConfig),
+  port = process.env.PORT;
 
 // logger
 app.use(morgan('dev'));
@@ -23,32 +23,29 @@ app.use(bodyParser.json());
 
 app.use('/api-docs', express.static(path.join(__dirname, '../public/api-docs')));
 
-//use the defined routes
+// use the defined routes
 app.use('/', routes);
-
-
 
 // connect to db
 // initializeDb( db => {
 
-  // // internal middleware
-  // app.use(middleware({ config, db }));
+// internal middleware
+// app.use(middleware({ config, db }));
 
-  // // api router
-  // app.use('/api', api({ config, db }));
+// api router
+// app.use('/api', api({ config, db }));
 
 // app.get('/api/v1', (req, res) => {
 //   res.send({msg: 'welcome'});
 // });
 
 app.get('/pool', (req, res) => {
-  pool.connect( (err,client,done) => {
-    if(err){
-      console.log("not able to get connection "+ err);
+  pool.connect((err) => {
+    if (err) {
+      console.log(`not able to get connection ${err}`);
       res.status(400).send(err);
       console.log(err);
-    }
-    else {
+    } else {
       console.log('successfully connected');
     }
   });
--- tests/sample.test.js
+++ tests/sample.test.js
@@ -1,21 +1,17 @@
-'use strict';
-
-import {expect} from 'chai';
+import { expect } from 'chai';
 import request from 'supertest';
-import app from '../index'
+import app from '../index';
 
 
-describe('test the default api endpoint', function() {
-
-  describe('#GET / vi route', function () {
-    it('should test the v1 route', function (done) {
+describe('test the default api endpoint', () => {
+  describe('#GET / vi route', () => {
+    it('should test the v1 route', (done) => {
       request(app).get('/api/v1/')
-        .end(function (err, res) {
+        .end((err, res) => {
           if (err) return done(err);
           expect(res.statusCode).to.equal(200);
-          expect(res.body.msg).to.equal('welcome');
           done();
-        })
-    })
+        });
+    });
   });
 });
