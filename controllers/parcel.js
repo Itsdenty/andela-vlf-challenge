b2a4b08a87120f8f969e162ca4ba7554ebbae01f
Abd-afeez Abd-hamid <dent4real@yahoo.com>
2018-11-14 10:11:15 +0000

    Add: get one parcel controller handler

--- controllers/parcel.js
+++ controllers/parcel.js
@@ -45,6 +45,24 @@ class parcelController {
       res.send(transformer.transformResponse(500, error.error));
     }
   }
+
+  /**
+   *
+   *
+   * @static
+   * @param {*} req
+   * @param {*} res
+   * @memberof parcelController
+   * @returns {json} oneParcel response
+   */
+  static async getOneParcel(req, res) {
+    try {
+      const oneParcel = await processor.getAllParcels();
+      res.send(transformer.transformResponse(200, oneParcel));
+    } catch (error) {
+      res.send(transformer.transformResponse(500, error.error));
+    }
+  }
 }
 
 export default parcelController;
