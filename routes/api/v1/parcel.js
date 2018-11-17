84a155ac8b3295f064a001bab539ad0f6cfb08f5
Abd-afeez Abd-hamid <dent4real@yahoo.com>
2018-11-17 06:22:50 +0000

    Add: change status endpoint validator

--- routes/api/v1/parcel.js
+++ routes/api/v1/parcel.js
@@ -7,7 +7,11 @@ import jwtVerify from '../../../middlewares/auth';
 const router = express.Router();
 router.post('/', jwtVerify.verifyToken, validator.create, controller.createParcel);
 router.get('/', jwtVerify.verifyToken, controller.getAllParcels);
-router.get('/:id', jwtVerify.verifyToken, validator.getOne, controller.getOneParcel);
-router.patch('/:id/cancel', jwtVerify.verifyToken, validator.getOne, controller.cancelParcelOrder);
-router.patch('/:id/destination', jwtVerify.verifyToken, validator.changeDestination, controller.changeParcelDestination);
+router.get('/:id', jwtVerify.verifyToken, validator.validadteId, controller.getOneParcel);
+router.patch('/:id/cancel', jwtVerify.verifyToken, validator.validadteId,
+  controller.cancelParcelOrder);
+router.patch('/:id/destination', jwtVerify.verifyToken,
+  validator.changeDestination, controller.changeParcelDestination);
+router.patch('/:id/status', jwtVerify.verifyToken,
+  validator.validateStatus, controller.changeParcelDestination);
 export default router;
