f6f738f4bd87d1d417c10f96bb7247bfc8c408ed
Abd-afeez Abd-hamid <dent4real@yahoo.com>
2018-11-24 07:12:16 +0000

    Update: basic parcel ui implemented

---
+++ ui/script/parcel.js
@@ -0,0 +1,185 @@
+'use strict';
+
+var _slicedToArray = function () { function sliceIterator(arr, i) { var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"]) _i["return"](); } finally { if (_d) throw _e; } } return _arr; } return function (arr, i) { if (Array.isArray(arr)) { return arr; } else if (Symbol.iterator in Object(arr)) { return sliceIterator(arr, i); } else { throw new TypeError("Invalid attempt to destructure non-iterable instance"); } }; }();
+
+/* eslint-disable no-undef, no-unused-vars */
+var currentModal = '',
+    loaderStatus = 0;
+
+var errorMessage = document.getElementsByClassName('error'),
+    parcelBtn = document.getElementById('submit-signup'),
+    createParcelBtn = document.getElementById('submit-login'),
+    parcelForm = document.getElementById('signupForm'),
+    toast = document.getElementById('toast'),
+    createRoute = 'https://andela-vlf.herokuapp.com/api/v1/auth/login',
+
+
+// algorithm for loader animation
+loader = function loader(id) {
+  switch (loaderStatus) {
+    case 0:
+      document.getElementById(id).innerText = 'loading.';
+      loaderStatus = 1;
+      break;
+    case 1:
+      document.getElementById(id).innerText = 'loading..';
+      loaderStatus = 2;
+      break;
+    case 2:
+      document.getElementById(id).innerText = 'loading...';
+      loaderStatus = 3;
+      break;
+    case 3:
+      document.getElementById(id).innerText = 'loading....';
+      loaderStatus = 0;
+      break;
+    default:
+      document.getElementById(id).innerText = 'loading.....';
+      loaderStatus = 0;
+  }
+},
+
+
+//  function for displaying toaster
+showToast = function showToast(toastClass, data) {
+  toast.classList.remove('hidden');
+  toast.classList.add(toastClass);
+  toast.innerHTML = '<p>' + data.substr(0, 50) + '</p>';
+  var flashError = setTimeout(function () {
+    toast.classList.add('hidden');
+    toast.classList.remove(toastClass);
+  }, 5000);
+},
+
+
+// function for toggling login and signup modal
+toggleModal = function toggleModal(e) {
+  var elem = e.target.getAttribute('data-modal');
+  if (currentModal && currentModal !== elem) {
+    document.getElementById(currentModal).classList.add('hidden');
+    document.getElementById(elem).classList.remove('hidden');
+  } else {
+    document.getElementById(elem).classList.toggle('hidden');
+  }
+  currentModal = elem;
+},
+
+
+// function for dismissing modal
+dismissModal = function dismissModal() {
+  if (currentModal) {
+    document.getElementById(currentModal).classList.add('hidden');
+    currentModal = null;
+  }
+},
+
+
+// function for page animation and modal script
+startAnimation = function startAnimation() {
+  var startAnim = setInterval(animateText, 3000);
+  var classname = document.getElementsByClassName('trigger');
+  Array.from(classname).forEach(function (element) {
+    element.addEventListener('click', toggleModal);
+  });
+  var dismissname = document.getElementsByClassName('dismiss');
+  Array.from(dismissname).forEach(function (element) {
+    element.addEventListener('click', dismissModal);
+  });
+},
+
+
+// create account method for signup
+createAccount = function createAccount(evt) {
+  evt.preventDefault();
+  var headers = new Headers({
+    'content-type': 'application/json'
+  }),
+      userDetails = {
+    firstName: signupForm.firstName.value,
+    lastName: signupForm.lastName.value,
+    otherNames: signupForm.otherNames.value,
+    username: signupForm.username.value,
+    isAdmin: signupForm.isAdmin.value,
+    email: signupForm.email.value,
+    password: signupForm.password.value
+  },
+      startLoader = setInterval(loader, 500, 'submit-signup');
+
+  fetch(signupRoute, {
+    method: 'POST',
+    body: JSON.stringify({ user: userDetails }),
+    headers: headers
+  }).then(function (res) {
+    return Promise.all([res.json(), res]);
+  }).then(function (_ref) {
+    var _ref2 = _slicedToArray(_ref, 2),
+        data = _ref2[0],
+        res = _ref2[1];
+
+    if (!res.ok) {
+      clearInterval(startLoader);
+      showToast('toast-red', data.error);
+      signupBtn.innerText = 'Signup';
+      return;
+    }
+    showToast('toast-green', 'registration successful');
+    signupBtn.innerText = 'Signup';
+    dismissModal();
+    currentModal = '';
+    // window.location.href = '/profile.html';
+    localStorage.setItem('token', data.data.token);
+    localStorage.setItem('user', JSON.stringify(data.data.user));
+  }).catch(function (error) {
+    return alert(error.message);
+  });
+},
+
+
+// create account method for signup
+loginUser = function loginUser(evt) {
+  evt.preventDefault();
+  var headers = new Headers({
+    'content-type': 'application/json'
+  }),
+      loginDetails = {
+    email: loginForm.email.value,
+    password: loginForm.password.value
+  },
+      startLoader = setInterval(loader, 500, 'submit-login');
+  console.log(loginDetails);
+  fetch(loginRoute, {
+    method: 'POST',
+    body: JSON.stringify({ login: loginDetails }),
+    headers: headers
+  }).then(function (res) {
+    return Promise.all([res.json(), res]);
+  }).then(function (_ref3) {
+    var _ref4 = _slicedToArray(_ref3, 2),
+        data = _ref4[0],
+        res = _ref4[1];
+
+    if (!res.ok) {
+      clearInterval(startLoader);
+      showToast('toast-red', data.error);
+      loginBtn.innerText = 'Login';
+      return;
+    }
+    showToast('toast-green', 'login successful');
+    loginBtn.innerText = 'Login';
+    dismissModal();
+    currentModal = '';
+    // window.location.href = '/profile.html';
+    localStorage.setItem('token', data.data.token);
+    localStorage.setItem('user', JSON.stringify(data.data.user));
+  }).catch(function (error) {
+    return alert(error.message);
+  });
+};
+
+// onload methods for ui animation and signup and login modal events
+window.onload = function () {
+  startAnimation();
+};
+
+// add event listeners
+createParcelForm.addEventListener('submit', createParcel);
